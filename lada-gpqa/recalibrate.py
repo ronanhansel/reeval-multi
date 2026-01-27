75b87dd2e63caf7b0caca063a8b343069d073615
Sang Truong <sttruong@cs.stanford.edu>
2026-01-27 16:14:02 +0000

    chore: cleanup

--- lada-gpqa/recalibrate.py
+++
@@ -1,5 +0,0 @@
-# Populate the recalibrate python file with similar idea to the test. But now I have a floating point 0-1 resmat instead of binary.
-
-# I want to select randomly half the agents to recalibrate the parameter of the lada model. And then I want to measure how good the ranking is using the reconstructed LADA
-
-# Idea is that LADA fits theta based on GPQA, then uses that theta to recalibrate items from 
